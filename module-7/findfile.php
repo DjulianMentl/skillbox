2417dcc668fdcda6da337f09d9bd5f8a8a270944
DjulianMentl <47448126+DjulianMentl@users.noreply.github.com>
2022-07-03 23:49:23 +0300

    task 7.5 completed

--- module-7/findfile.php
+++ module-7/findfile.php
@@ -1,35 +1,35 @@
 <?php
 
-$searchRoot = 'E:\OpenServer\domains\localhost';
-$searchName = 'readme.txt';
+$searchRoot = 'E:\OpenServer\domains\localhost\skillbox\module-7\test_search';
+$searchName = 'test.txt';
 $searchResult = [];
 
 /**
- * Поиск файоа пр названию в стартовой папке и во всех ее подпапках
+ * Поиск файла по названию в стартовой папке и во всех ее подпапках
  * @param string $startDir - стартовая папка
  * @param string $nameFile - название искомого файла
  * @param array $searchResult - массив в который сохраняются все результаты поиска
  * @return void
  */
-function findFile(string $startDir, string $nameFile, array &$searchResult)
+function findFile(string $startDir, string $nameFile, array &$searchResult): void
 {
-    //удаление из результатов scandir системных папок
-    $scanned_directory = array_diff(scandir($startDir), array('..', '.', '.git'));
-
-    foreach ($scanned_directory as $value) {
-        $newDirectory = $startDir . '\\' . $value;
-
-        // проверка на наличие папок в "новой" директории
+    //удаляем из результатов scandir системные папки и
+    //обходим итеративно все элементы массива содержащего строки с названием фалов и папок
+    foreach (array_diff(scandir($startDir), array('..', '.', '.git')) as $value) {
+        // проверка на наличие папок в директории
         // если найдена, повторный запуск функции
-        if (is_dir($newDirectory)) {
-           findFile($newDirectory, $nameFile, $searchResult);
-
+        if (is_dir($startDir . '\\' . $value)) {
+           findFile($startDir . '\\' . $value, $nameFile, $searchResult);
             // проверка на наличие искомого файла в директории
-            // если найден, записываем в массив
+            // если искомый файл найден, записываем в массив
         } elseif ($value === $nameFile) {
             $searchResult[] = $startDir . '\\' . $nameFile;
         }
     }
 }
 findFile($searchRoot, $searchName, $searchResult);
-var_dump($searchResult);
+//удаляем из массива пустые файлы
+$searchResult = array_filter($searchResult, 'filesize');
+//выводим на экран результат
+$searchResult ? print_r($searchResult) :
+                print_r("В директории $searchRoot и во всех ее подпапках нет непустых файлов $searchName");
