6124807ca30e1878ac03fadedfbd4b897d37f154
DjulianMentl <47448126+DjulianMentl@users.noreply.github.com>
2022-12-11 15:02:15 +0300

    update test-debug.php

--- module-15/test-debug.php
+++ module-15/test-debug.php
@@ -4,7 +4,21 @@ $testArray = [2, 23, 2, 5, 34, 6, 6, 88, 9];
 
 foreach ($testArray as $key => $item) {
     if ($item === 88) {
-        echo 'stop the cycle';
+//        echo 'stop the cycle';
         break;
     }
+}
+
+
+$mystring = 'dfdabc';
+$findme = 'A';
+$pos = strpos($mystring, $findme);
+
+// Заметьте, что используется ===.  Использование == не даст верного
+// результата, так как 'a' находится в нулевой позиции.
+if ($pos === false) {
+    echo "Строка '$findme' не найдена в строке '$mystring'";
+} else {
+    echo "Строка '$findme' найдена в строке '$mystring'";
+    echo " в позиции $pos";
 }
