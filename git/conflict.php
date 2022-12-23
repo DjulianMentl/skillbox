dfc0cd0533e7c2de7f0cd1b79cc5073113162fc6
DjulianMentl <emrindya@gmail.com>
2022-12-23 17:38:05 +0300

    fix email address

--- git/conflict.php
+++ git/conflict.php
@@ -4,6 +4,7 @@ $name = "Evgeniy";
 $lastName = "Ryndya";
 $age = 38;
 $profession = "Junior PHP Devoloper";
+$email = "emrin@gmail.com";
 $branch = 'master';
 echo 'test conflict branch Master. Check. Warning.'
 $branchMasterb = 13;
